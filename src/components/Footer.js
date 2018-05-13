e23d3d95f3ce8a06c4102b65314c7c5f7c6d2656
Rafael Cordones <rafael@cordones.me>
2018-05-13 19:39:53 +0200

    Footer: remove e-mail

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,6 @@ const Footer = () => (
     <footer className="small">
       <div className="container">
         <div id="bottom-space" />
-          <p className="float-left">
-            <Link to="mailto:info@carmen-marcos.art?subject=Carmen%20Marcos%20Art">info@carmen-marcos.art</Link>
-          </p>
           <p className="float-right">© 2018 Carmen Marcos</p>
       </div>
     </footer>
