c0457203b6d1dc661b0dd222132973cb60b74aff
Rafael Cordones <rafael@cordones.me>
2018-05-13 10:53:54 +0200

    ArtworkGrid

--- src/components/ArtworksGrid.js
+++ src/components/ArtworksGrid.js
@@ -3,6 +3,7 @@ import * as PropTypes from 'prop-types'
 import { withSiteData, withRouteData, Link } from 'react-static'
 import { Container, Row, Col } from 'reactstrap'
 import map from 'lodash/map'
+import chunk from 'lodash/chunk'
 import ArtworkCard from '../components/ArtworkCard'
 
 class ArtworkGrid extends Component {
@@ -13,18 +14,22 @@ class ArtworkGrid extends Component {
         artworks: []
     }
     render() {
+        const rows = chunk(this.props.artworks, 4)
+
         return (
-            <div className="artwork-grid album py-5">
+            <div className="artwork-grid album">
                 <Container>
-                    <Row> 
                     {
-                        map(this.props.artworks, (item, index) => (
-                            <Col key={index} xl="2" lg="2" md="3" sm="6">
-                                <ArtworkCard artwork={item}/>
-                            </Col>
+                        map(rows, (row, rowNumber) => (
+                            <Row key={rowNumber} className="py-4"> {
+                                map(row, (artworkItem, index) => (
+                                    <Col key={index} md="3" xs="12">
+                                        <ArtworkCard artwork={artworkItem}/>
+                                    </Col>
+                                ))}
+                            </Row>
                         ))
                     }
-                    </Row>
                 </Container>
             </div>
         )
