cc0789fe1722d2467cfec5d7a5946773b1047250
Rafael Cordones <rafael@cordones.me>
2018-05-15 10:12:44 +0200

    HomePage: remove exhibition images

--- src/components/ExhibitionAnnouncement.js
+++ src/components/ExhibitionAnnouncement.js
@@ -6,27 +6,25 @@ class ExhibitionAnnouncement extends Component {
         return (
             <div className="row mb-2">
                 <div className="col-md-6">
-                    <div className="card flex-md-row mb-4 box-shadow h-md-250">
+                    <div className="card flex-md-row mb-4 box-shadow h-md-125">
                         <div className="card-body d-flex flex-column align-items-start">
                             <strong className="d-inline-block mb-2 text-success">Exhibition</strong>
                             <h4 className="mb-0">Bild Wein Schau</h4>
-                            <div className="mb-1 text-muted">Saturday, May 26th @ 14:00</div>
+                            <div className="mb-1 text-muted">Saturday, May 26th @ 14:00 h.</div>
                             <a href="https://www.google.at/maps/place/Sterngasse+5,+2074+Unterretzbach/@48.7676147,16.0036343,182m/data=!3m1!1e3!4m5!3m4!1s0x476d57a8cc7ea4b1:0xa63dbbb925122f61!8m2!3d48.7676194!4d16.0040441">Sterngasse 5, 2074 Unterretzbach</a>
                         </div>
-                        <img className="card-img-right flex-auto d-none d-md-block" src="http://cms.casadelhuerto.com/api/v1/carmen-marcos-art/nodes/7f6bbb110c254633abbb110c25c6334b/binary/image?h=175"/>
-                        </div>
+                    </div>
                 </div>
                 <div className="col-md-6">
-                    <div className="card flex-md-row mb-4 box-shadow h-md-250">
+                    <div className="card flex-md-row mb-4 box-shadow h-md-125">
                         <div className="card-body d-flex flex-column align-items-start">
                             <strong className="d-inline-block mb-2 text-success">Exhibition</strong>
                             <h4 className="mb-0">Galerie Sandpeck</h4>
-                            <div className="mb-1 text-muted">Sunday, July 8th @ 14:00</div>
+                            <div className="mb-1 text-muted">Sunday, July 8th @ 18:00 h.</div>
                             <a href="https://www.google.com/maps/place/Florianigasse+75,+1080+Wien/@48.2119839,16.3387722,834m/data=!3m1!1e3!4m5!3m4!1s0x476d07e8fe6d8a01:0xe7159d49d3f1077!8m2!3d48.2119839!4d16.3409609">Florianigasse 75, 1080 Wien
 
 </a>
                         </div>
-                        <img className="card-img-right flex-auto d-none d-md-block" src="http://cms.casadelhuerto.com/api/v1/carmen-marcos-art/nodes/af907170527b477f907170527b977fd8/binary/image?h=175" />
                     </div>
                 </div>
             </div>
