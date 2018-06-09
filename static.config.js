27c6e4b0641121ad997aaf052b48dd7a02833c3d
Rafael Cordones <rafael@cordones.me>
2018-06-09 07:52:15 +0200

    Add possible fix for IE 11 rendering issues

--- static.config.js
+++ static.config.js
@@ -148,6 +148,7 @@ export default {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
